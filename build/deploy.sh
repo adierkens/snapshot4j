e6cfec390c9931c688f7cb1f3b68e7b842a9e6f1
Adam Dierkens <Adam_Dierkens@intuit.com>
2017-05-17 08:39:03 -0700

    Use gpg profile

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -Prelease --settings settings.xml
+    mvn deploy -Prelease -Pgpg --settings settings.xml
 fi
